a5b7cb91753f3094660ef4380ae20ff5f5998b9a
Ayush jha <119880444+Ayushjha1622@users.noreply.github.com>
2024-02-12 22:20:54 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,7 @@
+x=int(input("Enter first number: "))
+fac=1
+while x>0:
+    fac=fac*x
+    x-=1
+print(fac)
+    
---
+++ formattingString.py
@@ -0,0 +1,9 @@
+# Program to Format the string 
+a=10
+b=5
+c=15
+d=20
+str="a={},b={},c={},d={}"
+print(str.format(a,b,c,d))#isme sab apni hi value lenge 
+print(str.format(c,a,b,d))
+#jo str in variables likhe huye h wo value lete h format se i.e;iss program me str me jo a h wo c ki value lega and b a ki value legaand so on...
