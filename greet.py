5296d805018aafea7af883aeabdc5f7056590364
Ayush jha <119880444+Ayushjha1622@users.noreply.github.com>
2024-03-19 14:43:33 +0530

    Add files via upload

---
+++ greet.py
@@ -0,0 +1,9 @@
+def greet(name):
+    print("good day, "+name)
+greet("ayush")
+
+
+def sum(n1,n2):
+    return n1+n2
+s=sum(6,7)
+print(s)
---
+++ pr_08_08.py
@@ -0,0 +1,5 @@
+def multiply(n,range_l):
+    for i in range(1,range_l+1):
+        print(n,"X",i,"=",n*i)
+        
+multiply(5,10)
