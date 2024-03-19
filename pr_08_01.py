39c03f348abaf5c7fc0a21efa14a2ece41d669cc
Ayush jha <119880444+Ayushjha1622@users.noreply.github.com>
2024-03-19 14:42:43 +0530

    Add files via upload

---
+++ pr_08_01.py
@@ -0,0 +1,14 @@
+def maximum(n1,n2,n3):
+    if(n1>n2):
+        if(n1>n3):
+            return n1
+        else:
+            return n3
+    else:
+        if(n2>n3):
+            return n2
+        else:
+            return n3
+        
+m=maximum(2,3,4)
+print(m)
---
+++ pr_08_02.py
@@ -0,0 +1,6 @@
+def fahrenheit(cel):
+    return (cel*(9/5))+32
+
+c=0
+f=fahrenheit(c)
+print(str(f))
